414a6024ddd416d01bb713c5371f0ca9fd52e7c1
VictoriaKhr <18sweetcandy@mail.ru>
2026-03-15 15:08:26 +0500

    Add custom attributes: ResponseTime, StudentInfo, ValidateUserRequest

---
+++ TaskHub/Api/Attributes/ResponseTimeHeaderAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Attributes;
+
+
+public class ResponseTimeHeaderAttribute : ActionFilterAttribute
+{
+    private readonly Stopwatch stopwatch = new();
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        stopwatch.Start();
+        base.OnActionExecuting(context);
+    }
+
+    public override void OnActionExecuted(ActionExecutedContext context)
+    {
+        stopwatch.Stop();
+        context.HttpContext.Response.Headers.Append("X-Response-Time-Ms", stopwatch.ElapsedMilliseconds.ToString());
+        base.OnActionExecuted(context);
+    }
+}
